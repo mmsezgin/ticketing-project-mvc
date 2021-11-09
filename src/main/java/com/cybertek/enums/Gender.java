5b1cd2188776968a8b7f9e4ebc77d03f1dc13e38
Baros2535 <brsaltn.25@gmail.com>
2021-11-09 13:25:22 +0300

    enums,dto,entity packages are created

---
+++ src/main/java/com/cybertek/enums/Gender.java
@@ -0,0 +1,14 @@
+package com.cybertek.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Gender {
+
+    MALE("Male"),FEMALE("Female");
+private final String value;
+ private  Gender(String value) {
+        this.value=value;
+    }
+
+}
