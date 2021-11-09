4a17a80c7e22b540c49621e9ebce77a643cb510c
Toshiba <mehmetsezginm@gmail.com>
2021-11-09 20:55:26 +0000

    CRUD Service interface created.

--- src/main/java/com/cybertek/service/UserService.java
+++ src/main/java/com/cybertek/service/UserService.java
@@ -5,13 +5,9 @@ import com.cybertek.entity.User;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends CrudService<UserDTO, String>{
     //Describe business logic here
     // Get, save, delete all users etc.
-    UserDTO save (UserDTO user);
-    UserDTO findByID (String username);
-    List<UserDTO> findAll();
-    void delete (UserDTO user);
-    void deleteByID (String username);
+
 
 }
