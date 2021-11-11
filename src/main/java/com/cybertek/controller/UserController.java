f5915a1d71977c3b885ef10ec11aacab3b1e20a6
Toshiba <mehmetsezginm@gmail.com>
2021-11-11 12:17:36 +0000

    insertUser() created

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -1,23 +1,46 @@
 package com.cybertek.controller;
 
 import com.cybertek.dto.UserDTO;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/create")
     public String createUser (Model model){
         model.addAttribute("user", new UserDTO());
-        //datagenerator
-        //
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
         return "/user/create";
     }
 
+    @PostMapping("/create")
+    public String insertUser(UserDTO user, Model model){
+
+        //Save the entered user
+        userService.save(user);
+
+        //after insertion a fresh empty page wanted, so new UserDTO() needed.
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
+        //Since we want to see same user create page, return user create page.
+        return "/user/create";
+    }
 
 }
