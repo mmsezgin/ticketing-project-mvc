7f865741104b90f85f67e14c1fcd9186aa32092f
Toshiba <mehmetsezginm@gmail.com>
2021-11-15 22:44:11 +0000

    {#temporals.format established in create.html

---
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -0,0 +1,39 @@
+package com.cybertek.controller;
+
+import com.cybertek.dto.ProjectDTO;
+import com.cybertek.enums.Status;
+import com.cybertek.service.ProjectService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+    @Autowired
+    ProjectService projectService;
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("managers", userService.findAll());
+
+
+        return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(ProjectDTO project){
+        projectService.save(project);
+        project.setProjectStatus(Status.OPEN);
+        return "redirect:/project/create";
+    }
+}
