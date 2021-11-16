86f08bcaa10342a081a32312dfc2cc179093916e
Toshiba <mehmetsezginm@gmail.com>
2021-11-16 11:45:09 +0000

    TaskController class createTask() updated.

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.cybertek.controller;
 
 import com.cybertek.dto.TaskDTO;
+import com.cybertek.service.ProjectService;
+import com.cybertek.service.TaskService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class TaskController {
 
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    TaskService taskService
+
     @GetMapping("/create")
     public String createTask(Model model){
 
         model.addAttribute("task",new TaskDTO());
-
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees",userService.findAll());
+        model.addAttribute("tasks",taskService.findAll());
 
         return "task/create";
     }
