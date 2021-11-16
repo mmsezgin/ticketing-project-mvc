0b943005d968b118f3e14711fdd272a4c9103252
Toshiba <mehmetsezginm@gmail.com>
2021-11-16 11:53:43 +0000

    DataGenerator tasks data populated.

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/task")
 public class TaskController {
 
     @Autowired
